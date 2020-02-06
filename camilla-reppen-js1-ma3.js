d81e8c90e26cd3bec82ca60a71767356ae7a174b
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-02-06 13:38:30 +0100

    added more questions

--- camilla-reppen-js1-ma3.js
+++ camilla-reppen-js1-ma3.js
@@ -1,4 +1,9 @@
 // Question 1
+//Convert the function below to an arrow function:
+
+//function(a, b) {
+//    return a - b;
+//}
 
 //const sum = (a, b) => a - b;
 
@@ -8,7 +13,7 @@
 //pass the JSON it returns to a function and inside that function loop through the results and log each game's name.
 
 // In the catch method of your code, redirect to error.html if there is an error.
-
+/*
 
 fetch("https://api.rawg.io/api/games?genres=sports")
     .then(function(response) {
@@ -28,7 +33,44 @@ fetch("https://api.rawg.io/api/games?genres=sports")
             console.log(games[i].name);
         }
     }
+    
+
+// Question 3
+// Replace the word cats with the word giraffes in the following sentence:
+
+// These cats are outrageous.
+
+const animalReplace = animal.replace(/-/g, " ").replace("cate", "giraffes");
 
 
-        
 
+// Question 4
+//Given the URL below, write code that checks if there is a userId parameter in the query string.
+
+//If there is no userID parameter, redirect the browser to third.html.
+
+//If there is a userID parameter and its value is less than 10, redirect to first.html.
+
+//If there is a userID parameter and its value is 10 or greater, redirect to second.html.
+
+
+
+
+// Question 5
+// Write code that removes the button, and only the button, from its parent element in the HTML below:
+
+const container = document.querySelector(".container");
+const buttonRemove = document.querySelector(".btn");
+container.removeChild(buttonRemove);
+
+*/
+
+// Question 6
+//Create an li element with a text value of Parrots and a class of parrots.
+
+//Add the new item as the second item in the ul below (add it after Cows).
+
+
+const birdParrots = document.createElement("li");
+
+birdParrots.className = "parrots";
